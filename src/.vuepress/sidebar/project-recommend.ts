64f60ab7609b0a3faef473e2a0867a71841b844a
humeng <humeng_mail@163.com>
2023-09-23 13:37:39 +0800

    update

--- src/.vuepress/sidebar/project-recommend.ts
+++ src/.vuepress/sidebar/project-recommend.ts
@@ -3,7 +3,7 @@ import { arraySidebar } from "vuepress-theme-hope";
 export const project_recommend = arraySidebar([
     {
         text: "基础项目-瑞吉外卖",
-        link: "basis-project",
+        link: "basis-project-ruiji-tackout",
         icon: "code",
     },
 
